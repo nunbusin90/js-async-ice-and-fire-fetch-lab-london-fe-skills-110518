4eed070c5ea4de7ff17182c8dded499a461d65a4
NA YEON LEE <lee.nayeon90@gmail.com>
2019-01-07 20:25:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
   const url ="https://anapioficeandfire.com/api/books"
-  fetch(url).then
+  fetch(url).then(function(response){response.json()})
 }
 
 function renderBooks(json) {
